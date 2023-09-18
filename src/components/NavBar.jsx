672300f2a8b5f60b89d82ce9acd749d6e936a6ea
jessica <jessica.g.adamek@gmail.com>
2023-09-18 13:35:37 -0300

    :construction: create NavBar component structure

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,27 @@
+import { BsGithub } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+
+import "../styles/components/navBar.sass";
+
+import Logo from "../assets/logo.svg";
+
 export function NavBar() {
    return(
-      <nav>
-         navbar
-      </nav>
+      <div className="aside-bar">
+         <img src={Logo} alt="Jessica's Logo" />
+         <nav>
+            <ul>
+               <li>about me</li>   
+               <li>skills</li>   
+               <li>work</li>   
+               <li>contact</li>   
+            </ul>
+
+         </nav>
+         <div id="social-medias">
+            <BsGithub size={32} />
+            <BsLinkedin size={32} />
+         </div>
+      </div>
    )
 }
