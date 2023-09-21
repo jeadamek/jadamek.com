76a364c8295d4ba9b36ae991e4b5bba6a0ec65f4
jessica <jessica.g.adamek@gmail.com>
2023-09-21 16:03:52 -0300

    :art: create header structure

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,14 @@
 export function MainContent() {
    return (
-      <main>
-         main
-      </main>
+      <div id="page-content">
+         <header>
+            <h1>Jessica Adamek</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe odio quas laborum? Eos odit officia tempora natus quae, ducimus</p>
+         </header>
+         
+         <main>
+
+         </main>
+      </div>
    )
 }
