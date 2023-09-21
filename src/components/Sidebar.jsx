3ab0f4c0bd749a1f63152c7335443aae5b96f1dd
jessica <jessica.g.adamek@gmail.com>
2023-09-21 11:41:05 -0300

    :listick: make sidebar responsive

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { BsGithub } from "react-icons/bs";
 import { BsLinkedin } from "react-icons/bs";
+import { RiMenu5Fill } from "react-icons/ri";
 
 import "../styles/components/sidebar.sass";
 
@@ -8,7 +9,10 @@ import Logo from "../assets/logo.svg";
 export function Sidebar() {
    return(
       <div className="sidebar">
+         <span className="menu-btn"><RiMenu5Fill size={28} /></span>
+
          <img id="logo" src={Logo} alt="Jessica's Logo" />
+
          <nav>
             <ul>
                <li><a href="#">about me</a></li>   
@@ -16,8 +20,8 @@ export function Sidebar() {
                <li><a href="#">work</a></li>   
                <li><a href="#">contact</a></li>   
             </ul>
-
          </nav>
+         
          <div id="social-media">
             <a href="https://github.com/jeadamek" target="_blank" rel="noreferrer"><BsGithub size={28} /></a>
             <a href="https://www.linkedin.com/in/jessica-adamek/" target="_blank" rel="noreferrer"><BsLinkedin size={28} /></a>
