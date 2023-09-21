0504a6d3a9e1a90e323b6d8d9ffbbf48aa3cea92
jessica <jessica.g.adamek@gmail.com>
2023-09-21 18:20:54 -0300

    :construction: build mobile footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,12 +8,12 @@ export function Footer(){
       <footer>
          <div className="author-info">
             Designed and Developed by 
-            <span>Jessica Adamek 2023</span>
+            <div>Jessica Adamek 2023</div>
          </div>
 
          <div className="social-media">
-            <a href="https://github.com/jeadamek" target="_blank" rel="noreferrer"><BsGithub size={28} /></a>
-            <a href="https://www.linkedin.com/in/jessica-adamek/" target="_blank" rel="noreferrer"><BsLinkedin size={28} /></a>
+            <a href="https://github.com/jeadamek" target="_blank" rel="noreferrer"><BsGithub size={24} /></a>
+            <a href="https://www.linkedin.com/in/jessica-adamek/" target="_blank" rel="noreferrer"><BsLinkedin size={24} /></a>
          </div>
       </footer>
    )
