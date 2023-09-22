b3cffad69196c24a98f287a514e0f1dfbea430c4
jessica <jessica.g.adamek@gmail.com>
2023-09-22 12:19:56 -0300

    :art: create Section component props structure

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,12 +1,19 @@
+import PropTypes from 'prop-types';
+
 import { SectionTitle } from  "./SectionTitle";
 
 import "../styles/components/section.sass";
 
-export function Section() {
+export function Section({title, children}) {
    return (
       <section>
-         <SectionTitle title="title" />
-         
+         <SectionTitle title={title} />
+         {children}
       </section>
    )
 }
+
+Section.propTypes = {
+   title: PropTypes.string.isRequired,
+   children: PropTypes.any.isRequired
+};
