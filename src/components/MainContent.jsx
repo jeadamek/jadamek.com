2c0aa9c5543d63a1be1db9e21b7ee070e6d31665
jessica <jessica.g.adamek@gmail.com>
2023-09-22 14:26:37 -0300

    :sparkles: add and style Skills section

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -12,7 +12,52 @@ export function MainContent() {
             
             <main>
                <Section title="about me">
-                  <p className='info'>Lorem ipsum dolor sit amet <strong>consectetur</strong> adipisicing elit. Quibusdam numquam, mollitia cupiditate voluptate unde, iure saepe, soluta porro voluptatum nesciunt ex quas dolores cum illo totam excepturi dolore. Officiis, explicabo!</p>
+                  <p className="info">Lorem ipsum dolor sit amet <strong>consectetur</strong> adipisicing elit. Quibusdam numquam, mollitia cupiditate voluptate unde, iure saepe, soluta porro voluptatum nesciunt ex quas dolores cum illo totam excepturi dolore. Officiis, explicabo!</p>
+               </Section>
+
+               <Section title="skills">
+                  <div className="info" id="skills">
+                     <div>
+                        <h3>Languages</h3>
+                        <ul>
+                           <li>JavaScript</li>
+                           <li>HTML</li>
+                           <li>CSS</li>
+                           <li>Python</li>
+                           <li>SQL</li>
+                        </ul>
+                     </div>
+
+                     <div>
+                        <h3>Frameworks & Libraries</h3>
+                        <ul>
+                           <li>ReactJS</li>
+                           <li>NodeJS</li>
+                           <li>Bootstrap</li>
+                           <li>Restful API</li>
+                        </ul>
+                     </div>
+
+                     <div>
+                        <h3>Tools</h3>
+                        <ul>
+                           <li>Git & GitHub</li>
+                           <li>Chrome DevTolls</li>
+                           <li>Visual Studio Code</li>
+                        </ul>
+                     </div>
+
+                     <div>
+                        <h3>Design</h3>
+                        <ul>
+                           <li>UX/UI Design</li>
+                           <li>Figma</li>
+                           <li>Photoshop</li>
+                           <li>Prototyping</li>
+                           <li>Wireframing</li>
+                        </ul>
+                     </div>
+                  </div>
                </Section>
             </main>
 
