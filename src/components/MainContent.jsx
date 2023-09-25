ceed266071471e32f3cb761acf6e9e8f4a5c8f88
jessica <jessica.g.adamek@gmail.com>
2023-09-24 22:33:11 -0300

    :sparkles: textarea component is finished

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,121 +1,131 @@
-import { Header } from "./Header";
-import { Footer } from "./Footer";
-import { Project } from "./Project";
-import { Section } from "./Section";
-import { Textarea } from "./Textarea";
+import { Input } from './Input';
+import { Header } from './Header';
+import { Footer } from './Footer';
+import { Project } from './Project';
+import { Section } from './Section';
+import { Textarea } from './TextArea';
 
-import FoodExplorer from "../assets/imgs/food-explorer.png";
+import FoodExplorer from '../assets/imgs/food-explorer.png';
 
-import "../styles/components/mainContent.sass";
-import { Input } from "./Input";
+import '../styles/components/mainContent.sass';
 
 export function MainContent() {
-   return (
-      <div id="wrapper">
-         <div id="content-wrapper">
-            <Header />
-            
-            <main>
-               <Section title="about me">
-                  <p className="info">Lorem ipsum dolor sit amet <strong>consectetur</strong> adipisicing elit. Quibusdam numquam, mollitia cupiditate voluptate unde, iure saepe, soluta porro voluptatum nesciunt ex quas dolores cum illo totam excepturi dolore. Officiis, explicabo!</p>
-               </Section>
+  return (
+    <div id="wrapper">
+      <div id="content-wrapper">
+        <Header />
 
-               <Section title="skills" id="skills">
-                  <div className="info skills" >
-                     <div>
-                        <h3>Languages</h3>
-                        <ul>
-                           <li>JavaScript</li>
-                           <li>HTML</li>
-                           <li>CSS</li>
-                           <li>Python</li>
-                           <li>SQL</li>
-                        </ul>
-                     </div>
+        <main>
+          <Section title="about me">
+            <p className="info">
+              Lorem ipsum dolor sit amet <strong>consectetur</strong>{' '}
+              adipisicing elit. Quibusdam numquam, mollitia cupiditate voluptate
+              unde, iure saepe, soluta porro voluptatum nesciunt ex quas dolores
+              cum illo totam excepturi dolore. Officiis, explicabo!
+            </p>
+          </Section>
 
-                     <div>
-                        <h3>Frameworks & Libraries</h3>
-                        <ul>
-                           <li>ReactJS</li>
-                           <li>NodeJS</li>
-                           <li>Bootstrap</li>
-                           <li>Restful API</li>
-                        </ul>
-                     </div>
+          <Section title="skills" id="skills">
+            <div className="info skills">
+              <div>
+                <h3>Languages</h3>
+                <ul>
+                  <li>JavaScript</li>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>Python</li>
+                  <li>SQL</li>
+                </ul>
+              </div>
 
-                     <div>
-                        <h3>Tools</h3>
-                        <ul>
-                           <li>Git & GitHub</li>
-                           <li>Chrome DevTools</li>
-                           <li>Visual Studio Code</li>
-                        </ul>
-                     </div>
+              <div>
+                <h3>Frameworks & Libraries</h3>
+                <ul>
+                  <li>ReactJS</li>
+                  <li>NodeJS</li>
+                  <li>Bootstrap</li>
+                  <li>Restful API</li>
+                </ul>
+              </div>
 
-                     <div>
-                        <h3>Design</h3>
-                        <ul>
-                           <li>UX/UI Design</li>
-                           <li>Figma</li>
-                           <li>Photoshop</li>
-                           <li>Prototyping</li>
-                           <li>Wireframing</li>
-                        </ul>
-                     </div>
-                  </div>
-               </Section>
+              <div>
+                <h3>Tools</h3>
+                <ul>
+                  <li>Git & GitHub</li>
+                  <li>Chrome DevTools</li>
+                  <li>Visual Studio Code</li>
+                </ul>
+              </div>
 
-               <Section title="work" id="work">
-                  <div className="info work">
-                     <Project 
-                        title="Food Explorer"
-                        image={FoodExplorer}
-                        description="A web app designed for restaurants. Admins can easily manage dishes, orders, and customer information. Customers can create accounts, order food, and update their profiles with ease."
-                        techs={["react", "styled-components", "nodejs", "express"]}
-                     />
+              <div>
+                <h3>Design</h3>
+                <ul>
+                  <li>UX/UI Design</li>
+                  <li>Figma</li>
+                  <li>Photoshop</li>
+                  <li>Prototyping</li>
+                  <li>Wireframing</li>
+                </ul>
+              </div>
+            </div>
+          </Section>
 
-                     <Project 
-                        title="RocketMovies"
-                        image={FoodExplorer}
-                        description="This web app allows you to store and organize your movie ratings."
-                        techs={["react", "css", "nodejs", "express"]}
-                     />
-                  </div>
-               </Section>
+          <Section title="work" id="work">
+            <div className="info work">
+              <Project
+                title="Food Explorer"
+                image={FoodExplorer}
+                description="A web app designed for restaurants. Admins can easily manage dishes, orders, and customer information. Customers can create accounts, order food, and update their profiles with ease."
+                techs={['react', 'styled-components', 'nodejs', 'express']}
+              />
 
-               <Section title="contact" id="contact">
-                  <form className="info">
-                     <Input 
-                        id="name" 
-                        type="text"
-                        name="name"
-                        label="Name" 
-                        htmlFor="name" 
-                        required 
-                     />
-                     <Input 
-                        id="email" 
-                        type="email"
-                        name="email"
-                        label="Email" 
-                        htmlFor="email" 
-                        required
-                     />
-                     <Input 
-                        id="subject" 
-                        type="text"
-                        name="subject"
-                        label="Subject" 
-                        htmlFor="subject" 
-                        required
-                     />
-                     <Textarea />
-                  </form>
-               </Section>
-            </main>
+              <Project
+                title="RocketMovies"
+                image={FoodExplorer}
+                description="This web app allows you to store and organize your movie ratings."
+                techs={['react', 'css', 'nodejs', 'express']}
+              />
+            </div>
+          </Section>
 
-         </div>
-         <Footer />
+          <Section title="contact" id="contact">
+            <form className="info">
+              <Input
+                id="name"
+                type="text"
+                name="name"
+                label="Name"
+                htmlFor="name"
+                required
+              />
+              <Input
+                id="email"
+                type="email"
+                name="email"
+                label="Email"
+                htmlFor="email"
+                required
+              />
+              <Input
+                id="subject"
+                type="text"
+                name="subject"
+                label="Subject"
+                htmlFor="subject"
+                required
+              />
+              <Textarea
+                id="message"
+                name="message"
+                label="Message"
+                htmlFor="message"
+                required
+              />
+            </form>
+          </Section>
+        </main>
       </div>
-   )
+      <Footer />
+    </div>
+  )
 }
--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,11 +1,17 @@
+import PropTypes from 'prop-types'
 
-import "../styles/components/textarea.sass";
+import '../styles/components/textarea.sass'
 
-export function Textarea() {
-   return(
-      <div className="textarea-wrapper">
-         <textarea name="message" id="message" required ></textarea>
-         <label htmlFor="message">Message</label>
-      </div>
-   )
+export function Textarea({ label, htmlFor, ...rest }) {
+  return (
+    <div className="textarea-wrapper">
+      <textarea {...rest}></textarea>
+      <label htmlFor={htmlFor}>{label}</label>
+    </div>
+  )
+}
+
+Textarea.propTypes = {
+  label: PropTypes.string.isRequired,
+  htmlFor: PropTypes.string.isRequired
 }
