ade383a23936bb145dbd01abefefbd8f251ac371
jessica <jessica.g.adamek@gmail.com>
2023-09-28 15:56:08 -0300

    :construction: make nav fixed for desktop version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,39 +17,37 @@ export function Header() {
 
    return(
       <div className="sidebar">
-         <button className="menu-btn" 
-            onClick={() => {
-               handleModal()
-            }}>
-            <RiMenu5Fill size={28} />
-         </button>
-
-         <img id="logo" src={Logo} alt="Jessica's Logo" />
-
-         <div id="nav-wrapper">
-            <div className="buttons">
-               <button onClick={() => {
+         <header>
+            <button className="menu-btn"
+               onClick={() => {
                   handleModal()
                }}>
-                  <AiOutlineClose size={28}/>
-               </button>
-
-               <Toggle />
+               <RiMenu5Fill size={28} />
+            </button>
+            <img id="logo" src={Logo} alt="Jessica's Logo" />
+            <div id="nav-wrapper">
+               <div className="buttons">
+                  <button onClick={() => {
+                     handleModal()
+                  }}>
+                     <AiOutlineClose size={28}/>
+                  </button>
+                  <Toggle />
+               </div>
+               <nav>
+                  <ul>
+                     <li><a href="#">about me</a></li>
+                     <li className="active-menu"><a href="#">skills</a></li>
+                     <li><a href="#">work</a></li>
+                     <li><a href="#">contact</a></li>
+                  </ul>
+               </nav>
+               <div className="social-media">
+                  <a href="https://github.com/jeadamek" target="_blank" rel="noreferrer"><BsGithub size={28} /></a>
+                  <a href="https://www.linkedin.com/in/jessica-adamek/" target="_blank" rel="noreferrer"><BsLinkedin size={28} /></a>
+               </div>
             </div>
-            <nav>
-               <ul>
-                  <li><a href="#">about me</a></li>
-                  <li className="active-menu"><a href="#">skills</a></li>
-                  <li><a href="#">work</a></li>
-                  <li><a href="#">contact</a></li>
-               </ul>
-            </nav>
-
-            <div className="social-media">
-               <a href="https://github.com/jeadamek" target="_blank" rel="noreferrer"><BsGithub size={28} /></a>
-               <a href="https://www.linkedin.com/in/jessica-adamek/" target="_blank" rel="noreferrer"><BsLinkedin size={28} /></a>
-            </div>
-         </div>
+         </header>
       </div>
    )
 }
